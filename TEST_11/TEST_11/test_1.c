0e6c72b1eb5a63a74dfa8fad7b7a118ef004223d
CHINA-CS <759328360@qq.com>
2025-03-03 13:41:09 +0800

    第11次提交练习代码

---
+++ TEST_11/TEST_11/test_1.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//int main()
+//{
+//	int i = 1000;
+//	for (i = 1000; i <= 2000; i++)
+//	{
+//		if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+//		{
+//			printf("%d 是闰年！", i);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 100;
+//	int n = 2;
+//	for (i; i <= 200; i++)
+//	{
+//		if (i % n == 0)
+//		{
+//			printf
+//		}
+//
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 100;
+//	while (num <= 200)
+//	{
+//		int i = 2;
+//		while (i < num)
+//		{
+//			if (num % i == 0)
+//				break;
+//			i++;
+//		}
+//		if (i == num)
+//			printf("%d ", num);
+//		num++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 100;
+//	while (a <= 200)
+//	{
+//		int b = 2;
+//		while (b < a)
+//		{
+//			if (a % b == 0)
+//			{
+//				break;
+//			}
+//			b++;
+//		}
+//		if (b == a)
+//		{
+//			printf("%d 为质数", a);
+//		}
+//		a++;
+//	}
+//	return 0;
+//}
+
+
+int main()
+{
+
+	return 0;
+}
